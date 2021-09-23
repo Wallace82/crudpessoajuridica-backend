3b20b6276488ded0b1a3fe1fdafc11bc62d8317b
WALLACE GOMES DA SILVA <gomesw@gmail.com>
2021-09-23 08:24:54 -0300

    01 - Criando projeto com spring initializr

---
+++ src/main/java/com/desafio/crudpj/CrudpjApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.crudpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/crudpj/CrudpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.crudpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
