92f18446da322ce7719072f7293d11c4a49ce7a4
wallace.silva <gomesw@gmail.com>
2021-09-23 09:22:59 -0300

    03 - ENUM Tipo Empresa

--- src/main/java/com/desafio/crudpj/domain/TipoEmpresaEnum.java
+++ src/main/java/com/desafio/crudpj/domain/TipoEmpresaEnum.java
@@ -2,4 +2,23 @@ package com.desafio.crudpj.domain;
 
 public enum TipoEmpresaEnum {
 
+	MATRIZ(1,"Matriz"),
+	FILIAL(4,"Filial");
+	
+	private long id;
+	private String descricao;
+	
+	private TipoEmpresaEnum(long id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
 }
