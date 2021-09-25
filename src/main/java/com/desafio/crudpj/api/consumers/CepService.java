54929a0719b318f4f44f2b306047bb7ccc57d363
gomesw@gmail.com <gomesw@gmail.com>
2021-09-24 23:37:51 -0300

    08-Configurando Feign para consumir API via cep

---
+++ src/main/java/com/desafio/crudpj/api/consumers/CepService.java
@@ -0,0 +1,14 @@
+package com.desafio.crudpj.api.consumers;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.desafio.crudpj.api.dto.viacep.Endereco;
+
+@FeignClient(url= "https://viacep.com.br/ws/" , name = "viacep")
+public interface CepService {
+
+    @GetMapping("{cep}/json")
+    Endereco buscaEnderecoPorCep(@PathVariable("cep") String cep);
+}
